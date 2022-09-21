2c14cc04f9b6624e482000a48fd49c7c85b874f3
vcss7 <vs.victorsolis@gmail.com>
2022-09-21 16:05:44 -0700

    read_n done

--- VectorMultiplication/mpi_vect_mult.c
+++ VectorMultiplication/mpi_vect_mult.c
@@ -24,6 +24,7 @@ int main(void)
     return 0;
 }
 
+/* Reads user input for the size of the vectors */
 void Read_n(int* n_p, int* local_n_p, int my_rank, int comm_sz, MPI_Comm comm)
 {
     if (my_rank == 0)
@@ -36,13 +37,19 @@ void Read_n(int* n_p, int* local_n_p, int my_rank, int comm_sz, MPI_Comm comm)
 
         /* calculate local_n_elem using comm_sz and n */
         *local_n_p = *n_p / comm_sz;
+        assert(*local_n_p == (*n_p / comm_sz));
+    }
+ 
+    /* Proc = 0: send n and local_n to each process */
+    /* Proc > 0: receive n and local_n */
+    MPI_Bcast(n_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(local_n_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        printf("local_n_p = ", local_n_p);
-        
-        /* todo: send local_n to each process */
-        /* todo: implement via broadcast or send */
-        MPI_Bcast(n_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(local_n_p, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Process %d:\n", my_rank);
+    printf("\tn = %d:\n", *n_p);
+    printf("\tlocal_n = %d:\n", *local_n_p);
+}
+
     }
 }
 
