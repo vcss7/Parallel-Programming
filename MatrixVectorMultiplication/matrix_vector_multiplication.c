b1da5689423d7b423b0e2ff8d371d141988ca1ec
vcss7 <vs.victorsolis@gmail.com>
2022-10-24 21:05:02 -0700

    matrix_vector_multiplication: Read_matrix

--- MatrixVectorMultiplication/matrix_vector_multiplication.c
+++ MatrixVectorMultiplication/matrix_vector_multiplication.c
@@ -4,7 +4,7 @@
 #include <mpi.h>
 
 
-void Read_input(    int*            n_p             /* out */,
+void Read_n(        int*            n_p             /* out */,
                     int*            local_n_p       /* out */,
                     int             my_rank         /* in  */,
                     int             comm_sz         /* in  */,
@@ -22,12 +22,25 @@ void Print_vector(  int             local_vec[]     /* in  */,
                     MPI_Datatype    MPI_VECTOR      /* in  */,
                     MPI_Comm        comm            /* in  */);
 
+void Read_matrix(   int             local_matrix[]  /* out */,
+                    int             n               /* in  */,
+                    int             my_rank         /* in  */,
+                    MPI_Datatype    MPI_MATRIX      /* in  */,
+                    MPI_Comm        comm            /* in  */);
+
+void Print_matrix(  int             local_m[]       /* in  */,
+                    int             n               /* in  */,
+                    int             my_rank         /* in  */,
+                    MPI_Datatype    MPI_MATRIX      /* in  */,
+                    MPI_Comm        comm            /* in  */);
+
 int main(void)
 {
     /* initialize data */
     int n;
     int local_n;
     int* local_vec = NULL;
+    int* local_matrix = NULL;
 
     /* initialize mpi */
     int comm_sz;
@@ -42,22 +55,36 @@ int main(void)
     /* preogram begin */
     if (my_rank == 0)
     {
-        printf("Enter order of Vector and Matrix\n");
+        printf("Enter order of Vector and Matrix: ");
+        fflush(stdout);
     }
-    Read_input(&n, &local_n, my_rank, comm_sz, comm);
+    Read_n(&n, &local_n, my_rank, comm_sz, comm);
     local_vec = malloc(local_n * sizeof(int));
+    local_matrix = malloc(local_n * local_n * sizeof(int));
 
-    // custom mpi type
+    // custom mpi types
     MPI_Datatype MPI_VECTOR;
     MPI_Type_contiguous(local_n, MPI_INT, &MPI_VECTOR);
     MPI_Type_commit(&MPI_VECTOR);
 
-    // grab vector and matrix
+    MPI_Datatype MPI_MATRIX;
+    MPI_Type_contiguous(local_n * local_n, MPI_INT, &MPI_MATRIX);
+    MPI_Type_commit(&MPI_MATRIX);
+
+    // grab vector
     if (my_rank == 0) 
     {
-        printf("Enter Vector V\n");
+        printf("Enter Vector V: ");
+        fflush(stdout);
     }
     Read_vector(local_vec, n, my_rank, MPI_VECTOR, comm);
+
+    //grab matrix
+    if (my_rank == 0)
+    {
+        printf("Enter Matrix M\n");
+    }
+    Read_matrix(local_matrix, n, my_rank, MPI_MATRIX, comm);
 
     // print data to verify
     if (my_rank == 0)
@@ -66,11 +93,84 @@ int main(void)
     }
     Print_vector(local_vec, n, my_rank, MPI_VECTOR, comm);
 
+    if (my_rank == 0)
+    {
+        printf("Matrix M\n========\n");
+    }
+    Print_matrix(local_matrix, n, my_rank, MPI_MATRIX, comm);
+
     /* finalize mpi */
     free(local_vec);
     MPI_Finalize();
     return 0;
-}
+} /* main */
+
+
+void Print_matrix(
+    int             local_m[]       /* in  */,
+    int             n               /* in  */,
+    int             my_rank         /* in  */,
+    MPI_Datatype    MPI_MATRIX      /* in  */,
+    MPI_Comm        comm            /* in  */)
+{
+    int* m = NULL;
+    int i;
+    int j;
+
+    if (my_rank == 0)
+    {
+        m = malloc(n * n * sizeof(int));
+
+        MPI_Gather(local_m, 1, MPI_MATRIX, m, 1, MPI_MATRIX, 0, comm);
+
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                printf("%d ", m[i * n + j]);
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        MPI_Gather(local_m, 1, MPI_MATRIX, m, 1, MPI_MATRIX, 0, comm);
+    }
+} /* Print_matrix */
+
+void Read_matrix(
+    int             local_m[]       /* out */,
+    int             n               /* in  */,
+    int             my_rank         /* in  */,
+    MPI_Datatype    MPI_MATRIX      /* in  */,
+    MPI_Comm        comm            /* in  */)
+{
+    int* m = NULL;
+    int i;
+    int j;
+
+    if (my_rank == 0)
+    {
+        m = malloc(n * n * sizeof(int));
+        
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                scanf("%d", &m[i * n + j]);
+            }
+        }
+
+        MPI_Scatter(m, 1, MPI_MATRIX, local_m, 1, MPI_MATRIX, 0, comm);
+
+        free(m);
+    }
+    else
+    {
+        MPI_Scatter(m, 1, MPI_MATRIX, local_m, 1, MPI_MATRIX, 0, comm);
+    }
+} /* Read_matrix */
+
 
 void Print_vector(
     int             local_vec[]     /* in  */,
@@ -132,7 +232,7 @@ void Read_vector(
 } /* Read_vector */
 
 
-void Read_input(
+void Read_n(
     int*            n_p             /* out */,
     int*            local_n_p       /* out */,
     int             my_rank         /* in  */,
@@ -148,5 +248,5 @@ void Read_input(
 
     MPI_Bcast(n_p, 1, MPI_INT, 0, comm);
     MPI_Bcast(local_n_p, 1, MPI_INT, 0, comm);
-} /* Read_inpit */
+} /* Read_n */
 
