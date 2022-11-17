cdc19caf4a368330552202f7729f1196f34ee199
vcss7 <vs.victorsolis@gmail.com>
2022-11-16 20:20:40 -0800

    format pthread pi file

--- PThreadIntroduction/4.1_pth_pi_mutex.c
+++ PThreadIntroduction/4.1_pth_pi_mutex.c
@@ -19,21 +19,23 @@ void Usage(char* prog_name);
 double Serial_pi(long long n);
 
 int main(int argc, char* argv[]) {
-   long       thread;  /* Use long in case of a 64-bit system */
-   pthread_t* thread_handles;
-   double start, finish, elapsed;
+    long       thread;  /* Use long in case of a 64-bit system */
+    pthread_t* thread_handles;
+    double start, finish, elapsed;
 
-   /* Get number of threads from command line */
-   Get_args(argc, argv);
+    /* Get number of threads from command line */
+    Get_args(argc, argv);
 
-   return 0;
+    return 0;
 }  /* main */
+
 
 /*------------------------------------------------------------------*/
 void* Thread_sum(void* rank) {
    
-   return NULL;
+    return NULL;
 }  /* Thread_sum */
+
 
 /*------------------------------------------------------------------
  * Function:   Serial_pi
@@ -42,16 +44,17 @@ void* Thread_sum(void* rank) {
  * Return val: Estimate of pi using n terms of Maclaurin series
  */
 double Serial_pi(long long n) {
-   double sum = 0.0;
-   long long i;
-   double factor = 1.0;
+    double sum = 0.0;
+    long long i;
+    double factor = 1.0;
 
-   for (i = 0; i < n; i++, factor = -factor) {
+    for (i = 0; i < n; i++, factor = -factor) {
       sum += factor/(2*i+1);
-   }
-   return 4.0*sum;
+    }
+    return 4.0*sum;
 
 }  /* Serial_pi */
+
 
 /*------------------------------------------------------------------
  * Function:    Get_args
@@ -60,11 +63,11 @@ double Serial_pi(long long n) {
  * Globals out: thread_count, n
  */
 void Get_args(int argc, char* argv[]) {
-   if (argc != 3) Usage(argv[0]);
-   thread_count = strtol(argv[1], NULL, 10);  
-   if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
-   n = strtoll(argv[2], NULL, 10);
-   if (n <= 0) Usage(argv[0]);
+    if (argc != 3) Usage(argv[0]);
+    thread_count = strtol(argv[1], NULL, 10);  
+    if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
+    n = strtoll(argv[2], NULL, 10);
+    if (n <= 0) Usage(argv[0]);
 }  /* Get_args */
 
 
@@ -74,8 +77,8 @@ void Get_args(int argc, char* argv[]) {
  * In arg:    prog_name
  */
 void Usage(char* prog_name) {
-   fprintf(stderr, "usage: %s <number of threads> <n>\n", prog_name);
-   fprintf(stderr, "   n is the number of terms and should be >= 1\n");
-   fprintf(stderr, "   n should be evenly divisible by the number of threads\n");
-   exit(0);
+    fprintf(stderr, "usage: %s <number of threads> <n>\n", prog_name);
+    fprintf(stderr, "   n is the number of terms and should be >= 1\n");
+    fprintf(stderr, "   n should be evenly divisible by the number of threads\n");
+    exit(0);
 }  /* Usage */
