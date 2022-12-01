f9ad434ca7a393b2376a5c9f3e4978bc2958fc00
vcss7 <vs.victorsolis@gmail.com>
2022-12-01 09:02:32 -0800

    init commit of pi estimate

--- MorePThreads/monte_carlo_pi_estimate.c
+++ MorePThreads/monte_carlo_pi_estimate.c
@@ -1,71 +1,31 @@
 #include <assert.h>
 #include <math.h>
-#include <mpi.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+const int MAX_THREADS = 1024;
 
-void Read_n(        int*            n_p         /* out */,
-                    int*            local_n_p   /* out */,
-                    int             my_rank     /* in  */,
-                    int             comm_sz     /* in  */,
-                    MPI_Comm        comm        /* in  */);
+int num_tosses;
+int num_in_circle;
+double pi_estimate;
 
-void Toss(          int             local_n     /* in  */,
-                    int*            local_count /* out */,
-                    int             my_rank     /* in  */,
-                    int             comm_sz     /* in  */,
-                    MPI_Comm        comm        /* in  */);
+void Read_n();
+void Pth_toss();
 
 
 int main(void)
 {
     /* initizalize data */
-    int n;
-    int local_n;
-    int local_count;
-    int count;
-    double pi_estimate;
-
-    /* initialize mpi */
-    int my_rank;
-    int comm_sz;
-
-    MPI_Comm comm = MPI_COMM_WORLD;
-
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(comm, &comm_sz);
-    MPI_Comm_rank(comm, &my_rank);
 
     /* program */
-    Read_n(&n, &local_n, my_rank, comm_sz, comm);
-    
-    Toss(local_n, &count, my_rank, comm_sz, comm);
-
-    if (my_rank == 0)
-    {
-        printf("count is %d\n", count);
-
-        pi_estimate = 4 * count / ((double) n); 
-
-        printf("pi_estimate is %lf\n", pi_estimate);
-    }
-    /* free allocated data */
-
-    /* finalize mpi */
-    MPI_Finalize();
+    Read_n();
 
     return 0;
 } /* main */
 
-
-void Toss(
-    int             local_n     /* in  */,
-    int*            count_p     /* out */,
-    int             my_rank     /* in  */,
-    int             comm_sz     /* in  */,
-    MPI_Comm        comm        /* in  */)
+void Pth_toss(void my_rank)
 {
     srand(1234321 + my_rank);
 
@@ -92,25 +52,14 @@ void Toss(
 
     MPI_Reduce(&local_count, count_p, 1, MPI_INT, MPI_SUM, 0, comm);
 }
+*/
 
 
-void Read_n(
-    int*            n_p         /* out */,
-    int*            local_n_p   /* out */,
-    int             my_rank     /* in  */,
-    int             comm_sz     /* in  */,
-    MPI_Comm        comm        /* in  */)
+void Read_n()
 {
-    if (my_rank == 0)
-    {
-        printf("Enter the number of tosses: ");
-        fflush(stdout);
-        scanf("%d", n_p);
+    printf("Enter the number of tosses: ");
+    fflush(stdout);
 
-        *local_n_p = *n_p / comm_sz;
-    }
-
-    MPI_Bcast(n_p, 1, MPI_INT, 0, comm);
-    MPI_Bcast(local_n_p, 1, MPI_INT, 0, comm);
+    scanf("%d", &num_tosses);
 }
     
