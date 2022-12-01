dde5ef08fe2bb32a8323f518562c785c50a4a9bb
vcss7 <vs.victorsolis@gmail.com>
2022-12-01 12:28:04 -0800

    pthread historgram done

--- MorePThreads/histogram.c
+++ MorePThreads/histogram.c
@@ -10,6 +10,16 @@ const int MAX_THREADS = 1024;
 int thread_count;
 pthread_mutex_t mutex;
 
+int bin_count, local_bin_count, *bin_counts;
+float min_meas, max_meas;
+int data_count, local_data_count;
+float* data;
+float* bin_mins, * bin_maxes;
+/*
+        float bin_mins[], float bin_maxes[], int bin_count, 
+        int bin_counts[], float min_meas, float max_meas);
+*/
+
 void Usage(char* prog_name);
 void Get_args(int argc, char* argv[]);
 void Read_input(int *bin_count_p, int *local_bin_count_p, int *data_count_p,
@@ -17,70 +27,119 @@ void Read_input(int *bin_count_p, int *local_bin_count_p, int *data_count_p,
 
 void Generate_data(int data_count, float data[], float min_meas, float max_meas);
 void Print_data(float data[], int data_count, char title[]);
-
-/*
 void Set_subintervals(float bin_mins[], float bin_maxes[], float min_meas, 
-        float max_meas, int bin_count, int my_rank, MPI_Comm comm);
-void Build_histogram(float local_data[], int local_data_count, float bin_mins[],
-        float bin_maxes[], int bin_count, int bin_counts[], float min_meas,
-        float max_meas);
+        float max_meas, int bin_count);
+void* Build_histogram(void* rank);
 void Print_histogram(float bin_mins[], float bin_maxes[], int bin_count,
-        int bin_counts[], int my_rank, MPI_Comm comm);
-*/
+        int bin_counts[]);
 
 
 int main(int argc, char* argv[])
 {
-    int bin_count, local_bin_count, *bin_counts;
-    float min_meas, max_meas;
-    int data_count, local_data_count;
-    float* data, *local_data;
-    float* bin_mins, * bin_maxes;
-    //int* bin_counts, local_bin_counts;
-
-    /*
-    MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-    */
-
-    /* Start of Program */
+    long thread;
+    pthread_t* thread_handles;
 
     Get_args(argc, argv);
     Read_input(&bin_count, &local_bin_count, &data_count, &local_data_count,
             &min_meas, &max_meas);
 
+    /* allocate data */
     data = malloc(data_count * sizeof(float));
-    Generate_data(data_count, data,  min_meas, max_meas);
-    Print_data(data, data_count, "init data");
-
-    /*
-    local_data = malloc(local_data_count * sizeof(float));
     bin_mins = malloc(bin_count * sizeof(float));
     bin_maxes = malloc(bin_count * sizeof(float));
     bin_counts = malloc(bin_count * sizeof(int));
-    local_bin_count = malloc(bin_count * sizeof(float));
+
+    Generate_data(data_count, data,  min_meas, max_meas);
+
+    Set_subintervals(bin_mins, bin_maxes, min_meas, max_meas, bin_count);
 
     for (int i = 0; i < bin_count; i++)
     {
         bin_counts[i] = 0;
     }
 
+    thread_handles = malloc(thread_count * sizeof(pthread_t));
 
-    Set_subintervals(bin_mins, bin_maxes, min_meas, max_meas, bin_count, my_rank, MPI_COMM_WORLD);
-    Build_histogram(local_data, local_data_count, bin_mins, bin_maxes,
-            bin_count, bin_counts, min_meas, max_meas);
+    for (thread = 0; thread < thread_count; thread++)
+    {
+        pthread_create(&thread_handles[thread], NULL, Build_histogram, (void*) thread);
+    }
 
-    Print_data(local_data, local_data_count, data_count, "histogram data", my_rank, MPI_COMM_WORLD);
-    Print_histogram(bin_mins, bin_maxes, bin_count, bin_counts, my_rank, MPI_COMM_WORLD);
+    for (thread = 0; thread < thread_count; thread++)
+    {
+        pthread_join(thread_handles[thread], NULL);
+    }
+
+    Print_data(data, data_count, "histogram data");
+    Print_histogram(bin_mins, bin_maxes, bin_count, bin_counts);
+
+    /*
+    local_data = malloc(local_data_count * sizeof(float));
+    local_bin_count = malloc(bin_count * sizeof(float));
     */
 
-
-    //MPI_Finalize();
-
     free(data);
+    free(bin_mins);
+    free(bin_maxes);
+    free(bin_counts);
+    free(thread_handles);
     return 0;
 } /* main */
+
+
+void Print_histogram(float bin_mins[], float bin_maxes[], int bin_count,
+        int bin_counts[])
+{
+    int i;
+
+    for (i = 0; i < bin_count; i++)
+    {
+        printf("bin %d (%f - %f] %d\n", i, bin_mins[i], bin_maxes[i], bin_counts[i]);
+    }
+}
+
+
+void* Build_histogram(void* rank)
+{
+    long my_rank = (long) rank;
+    int i;
+    int j;
+
+    int start_elem;
+    int stop_elem;
+    start_elem = (data_count / thread_count) * my_rank;
+    stop_elem = start_elem + local_data_count;
+    // go through each datum
+    for (i = 0; i < local_data_count; i++)
+    {
+        // go through each bin
+        for (j = 0; j < bin_count; j++)
+        {
+            if (bin_mins[j] < data[start_elem + i] && data[start_elem + i] <= bin_maxes[j])
+            {
+                pthread_mutex_lock(&mutex);
+                bin_counts[j]++;
+                pthread_mutex_unlock(&mutex);
+            }
+        }
+    }
+
+    return NULL;
+}
+
+
+void Set_subintervals(float bin_mins[], float bin_maxes[], float min_meas, 
+        float max_meas, int bin_count)
+{
+    float interval = (max_meas - min_meas) / (float) bin_count;
+
+    int i;
+    for (i = 0; i < bin_count; i++)
+    {
+        bin_mins[i] = min_meas + interval * (float) (i);
+        bin_maxes[i] = min_meas + interval * (float) (i + 1);
+    }
+}
 
 
 void Print_data(
@@ -137,62 +196,7 @@ void Read_input(
     printf("Each thread will sort %d numbers out of %d\n", *local_bin_count_p, *bin_count_p);
 }
 
-/*
-void Print_histogram(float bin_mins[], float bin_maxes[], int bin_count,
-        int bin_counts[], int my_rank, MPI_Comm comm)
-{
-    int i;
 
-    int *histogram_bin = malloc(bin_count * sizeof(int));
-
-    for (i = 0; i < bin_count; i++)
-    {
-        histogram_bin[i] = 0;
-        MPI_Reduce(&bin_counts[i], &histogram_bin[i], 1, MPI_INT, MPI_SUM, 0, comm);
-        if (my_rank == 0)
-        {
-            printf("bin %d (%f - %f] %d\n", i, bin_mins[i], bin_maxes[i], histogram_bin[i]);
-        }
-    }
-}
-
-
-void Build_histogram(float local_data[], int local_data_count, float bin_mins[],
-        float bin_maxes[], int bin_count, int bin_counts[], float min_meas,
-        float max_meas)
-{
-    int i;
-    int j;
-
-    // go through each datum
-    for (i = 0; i < local_data_count; i++)
-    {
-        // go through each bin
-        for (j = 0; j < bin_count; j++)
-        {
-            if (bin_mins[j] < local_data[i] && local_data[i] <= bin_maxes[j])
-            {
-                bin_counts[j]++;
-            }
-        }
-    }
-
-}
-
-
-void Set_subintervals(float bin_mins[], float bin_maxes[], float min_meas, 
-        float max_meas, int bin_count, int my_rank, MPI_Comm comm)
-{
-    float interval = (max_meas - min_meas) / (float) bin_count;
-
-    int i;
-    for (i = 0; i < bin_count; i++)
-    {
-        bin_mins[i] = min_meas + interval * (float) (i);
-        bin_maxes[i] = min_meas + interval * (float) (i + 1);
-    }
-}
-*/
 
 
 void Get_args(int argc, char* argv[]) {                                         
