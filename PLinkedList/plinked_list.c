01fbb0789fd00222f2abf6d94788b38fda0b72c8
vcss7 <vs.victorsolis@gmail.com>
2022-12-12 18:35:31 -0800

    pthread linked list: gen rand sorted list

--- PLinkedList/plinked_list.c
+++ PLinkedList/plinked_list.c
@@ -7,10 +7,21 @@
 const uint16_t MAX_THREADS = 1024;
 uint16_t num_threads;
 
+/* linked list data types */
+struct list_node_s {
+    int data;
+    struct list_node_s* next;
+};
+
 /* linked list variables */
+const uint16_t NUM_ITEMS_INIT = 50;
 uint16_t num_tasks;
+struct list_node_s* head_p;
 
 /* function forward declarations */
+int insert(int value, struct list_node_s** head_pp);
+void print_list();
+void gen_rand_sorted_linked_list();
 void get_args(int argc, char* argv[]);
 void usage(char* prog_name);
 
@@ -20,7 +31,89 @@ int main(int argc, char* argv[])
 {
     get_args(argc, argv);
 
+    gen_rand_sorted_linked_list();
+
+    print_list();
+
     return 0;
+} /* main */
+
+
+/* -----------------------------------------------------------------------------
+ * Function:    insert
+ * Purpose:     insert a linked list node into a linked list
+ * In Args:     value
+ *              head_pp
+ * Out Args:    a linked list node
+ */
+int insert(int value, struct list_node_s** head_pp)
+{
+    struct list_node_s* curr_p = *head_pp;
+    struct list_node_s* pred_p = NULL;
+    struct list_node_s* temp_p;
+
+    while (curr_p != NULL && curr_p->data < value)
+    {
+        pred_p = curr_p;
+        curr_p = curr_p->next;
+    }
+
+    if (curr_p == NULL || curr_p->data > value)
+    {
+        temp_p = malloc(sizeof(struct list_node_s));
+        temp_p->data = value;
+        temp_p->next = curr_p;
+
+        if (pred_p == NULL) /* New first node */
+        {
+            *head_pp = temp_p;
+        }
+        else
+        {
+            pred_p->next = temp_p;
+        }
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/* -----------------------------------------------------------------------------
+ * Function:    print_list
+ * Purpose:     print the linked list to stdout
+ * In Args:     none
+ * Out Args:    none
+ */
+void print_list()
+{
+    struct list_node_s* curr_p = head_p;
+
+    while (curr_p != NULL)
+    {
+        printf("%d ", curr_p->data);
+        curr_p = curr_p->next;
+    }
+    printf("\n");
+}
+
+
+/* -----------------------------------------------------------------------------
+ * Function:    gen_rand_sorted_linked_list
+ * Purpose:     generate an initial sorted linked list with random values
+ * In Args:     None
+ * Out Args:    linked_list
+ */
+void gen_rand_sorted_linked_list()
+{
+    int new_value;
+#pragma unroll
+    for (int i = 0; i < NUM_ITEMS_INIT; i++)
+    {
+        new_value = rand() % 100;
+        insert(new_value, &head_p);
+    }
 }
 
 
@@ -50,7 +143,7 @@ void get_args(int argc, char* argv[])
     {
         usage(argv[0]);
     }
-}
+} /* get_args */
 
 
 /* -----------------------------------------------------------------------------
